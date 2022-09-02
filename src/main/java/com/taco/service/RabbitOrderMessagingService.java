5602105ccbfd847a550af71abc2c6aadc8e97378
Satyam Joshi <satyam_joshi@epam.com>
2022-09-02 11:41:06 +0530

    using RabbitMQ for sending Message.

---
+++ src/main/java/com/taco/service/RabbitOrderMessagingService.java
@@ -0,0 +1,28 @@
+package com.taco.service;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taco.model.Order;
+
+@Service
+public class RabbitOrderMessagingService implements OrderMessagingService {
+	@Autowired
+	RabbitTemplate rabbit;
+
+	@Override
+	public void sendOrder(Order order) {
+		/*
+		 * MessageConverter converter = rabbit.getMessageConverter(); MessageProperties
+		 * props = new MessageProperties(); Message message = converter.toMessage(order,
+		 * props); rabbit.send("tacocloud.order", message);
+		 */
+		// replace above by
+		rabbit.convertAndSend("tacocloud.order", order);
+	}
+
+}
