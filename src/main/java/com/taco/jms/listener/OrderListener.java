db6a8021f0a32da339aac8f1c49a32d5335086a4
Satyam Joshi <satyam_joshi@epam.com>
2022-09-14 14:33:39 +0530

    Listening to message with Kafka

--- src/main/java/com/taco/jms/listener/OrderListener.java
+++ src/main/java/com/taco/jms/listener/OrderListener.java
@@ -2,6 +2,7 @@ package com.taco.jms.listener;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.taco.kitchen.KitchenUI;
@@ -15,6 +16,11 @@ public class OrderListener {
 	// @JmsListener(destination = "tacocloud.order.queue")
 	@RabbitListener(queues = "tacocloud.order.queue")
 	public void receiveOrder(Order order) {
+		kitchenUI.displayOrder(order);
+	}
+
+	@KafkaListener(topics = "tacocloud.orders.topic")//this method subscribes to this topic and listens for any changes.
+	public void handle(Order order) {
 		kitchenUI.displayOrder(order);
 	}
 
