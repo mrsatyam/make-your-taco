3489253a9e3a14d643a2be8746a3f13aa41b05d8
Satyam Joshi <satyam_joshi@epam.com>
2022-09-14 14:20:05 +0530

    Sending message through Kafka with topic name in send() method

---
+++ src/main/java/com/taco/service/KafkaOrderMessagingService.java
@@ -0,0 +1,18 @@
+package com.taco.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import com.taco.model.Order;
+
+public class KafkaOrderMessagingService implements OrderMessagingService {
+	
+	@Autowired
+	private KafkaTemplate<String, Order> kafkaTemplate;
+
+	@Override
+	public void sendOrder(Order order) {
+		kafkaTemplate.send("tacocloud.orders.topic", order);
+	}
+
+}
